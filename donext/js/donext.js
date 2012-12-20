615440b0f26c322715d43277b4f502a61587f66b
Sean Nelson <partlysean@gmail.com>
2012-12-20 01:48:56 -0500

    Drill down to lists
    - Drill down to todo list items
    - Add new list/todo form updates
    - Code refactoring
    - Cool animations
    - Other bits

--- donext/js/donext.js
+++ donext/js/donext.js
@@ -24,33 +24,37 @@ Session.set('showingTodoItems');
 /* Globals
 ------------------------------------------------------------ */
 
-var listItemsEle;
-var curX;
+// Global variables here
 
 /* Running on the client...
 ------------------------------------------------------------ */
 
 if (Meteor.isClient) {
-	// DOM is ready
-	Meteor.startup(function() {
-		listItemsEle = document.getElementById('list-items');
-	});
+	/* Show or hide items
+	------------------------------ */
 	
-	// Show or hide list items
+	Template.todo_lists.preserve(['#todo-lists']); // Preserved for animation
 	Template.list_items.preserve(['#list-items']); // Preserved for animation
 	
-	Template.list_items.isShowing = function() {
-		// return Session.equals('showingTodoItems', true) ? 'slide-view' : '';
+	Template.todo_lists.isShowing = function() {
+		return Session.equals('showingTodoItems', true) ? 'slide-view' : '';
 	};
 	
-	// Return all todo lists
+	Template.list_items.isShowing = function() {
+		return Session.equals('showingTodoItems', true) ? 'slide-view' : '';
+	};
+	
+	/* Todo lists
+	------------------------------ */
+	
+	// Return items from DB
 	Template.todo_lists.lists = function() {
 		return Lists.find({}, { sort: { dateCreated: -1 } });
 	};
 	
-	// Todo List Template Events
+	// Template events
 	Template.todo_lists.events({
-		'click': function(event) {
+		'click a': function(event) {
 			// Prevent default link action
 			event.preventDefault();
 			
@@ -62,56 +66,50 @@ if (Meteor.isClient) {
 		}
 	});
 	
-	// Add new todo list
-	Template.add_todo_list.events({
-		'submit': function(event) {
-			// Prevent default form action
-			event.preventDefault();
-			
-			// Save the input data
-	    	var inputData = document.addTodoListForm.addTodoListInput.value;
-	    	
-	    	// Add new todo
-	    	Lists.insert({
-	    		name: inputData,
-	    		dateCreated: new Date()
-	    	});
-	    	
-	    	// Clear field value
-	    	document.addTodoListForm.addTodoListInput.value = '';
-		}
-	});
+	/* Todo items
+	------------------------------ */
 	
-	// Add new todo item
-	Template.add_todo_item.events({
-		'submit': function(event) {
-			// Prevent default form action
-			event.preventDefault();
-			
-			// Save the input data
-	    	var inputData = document.addTodoItemForm.addTodoItemInput.value;
-	    	
-	    	// If a list is selected...
-	    	if (Session.get('currentTodoList')) {
-	    		// Add new todo
-		    	Items.insert({
-		    		description: inputData,
-		    		parentListId: Session.get('currentTodoList'),
-		    		dateCreated: new Date() });
-		    	
-		    	// Clear field value
-		    	document.addTodoItemForm.addTodoItemInput.value = '';
-		    }
-		    else {
-		    	alert('You don\'t have a list selected');
-		    }
-		}
-	});
-	
-	// Return currently selected todo list items	
+	// Return items from DB
 	Template.list_items.items = function() {
 		return Items.find({ 'parentListId': Session.get('currentTodoList') });
 	};
+	
+	/* Add new form
+	------------------------------ */
+	
+	Template.add_new.addInputPlaceholder = function() {
+		return Session.equals('showingTodoItems', true) ? 'Start typing to create a new todo...' : 'Start typing to create a new list...';
+	}
+	
+	Template.add_new.events({
+		'submit': function(event) {
+			// Prevent default form action
+			event.preventDefault();
+			
+			// Get the input value
+			var inputData = document.addNew.addNew.value;
+			
+			// If we're looking at todo lists...
+			if (Session.equals('showingTodoItems')) {
+				// Create a new todo list
+				Lists.insert({
+	    		    name: inputData,
+	    		    dateCreated: new Date()
+	    		});
+			}
+			else {
+				// Create a new todo item
+				Items.insert({
+		    		description: inputData,
+		    		parentListId: Session.get('currentTodoList'),
+		    		dateCreated: new Date()
+		    	});
+			}
+			
+			// Clear field value
+	    	document.addNew.addNew.value = '';
+		}
+	});
 }
 
 /* Running on the server...
